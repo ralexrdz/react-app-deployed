7b1cb087f7c5e94a2ab52903053dfc13d67a8381
ralex <ralex.rdz@gmail.com>
2019-01-12 14:48:35 -0600

    Updates

---
+++ precache-manifest.3a9530f4d2a3168e16bf3ffb50c82948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d8b853b943f589d6f7a",
+    "url": "/react-app-deployed/static/css/main.af3ec738.chunk.css"
+  },
+  {
+    "revision": "3d8b853b943f589d6f7a",
+    "url": "/react-app-deployed/static/js/main.3d8b853b.chunk.js"
+  },
+  {
+    "revision": "0fe7f01ed6544d3a6250",
+    "url": "/react-app-deployed/static/js/1.0fe7f01e.chunk.js"
+  },
+  {
+    "revision": "58454b865d77ae4236e4",
+    "url": "/react-app-deployed/static/js/runtime~main.58454b86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-deployed/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c6b6ced1457e194fc516d1aad1fa24c",
+    "url": "/react-app-deployed/index.html"
+  }
+];
