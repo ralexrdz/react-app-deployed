88eca13ed139159b1e16f1dec738f193e82717e2
ralex <ralex.rdz@gmail.com>
2019-01-12 14:34:37 -0600

    pre deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Axios from 'axios'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      saludo: 'aloha'
+    }
+  }
+  componentDidMount() {
+    Axios.get('https://replit-node-deploy--ralexrdz.repl.co/')
+      .then(data => {
+        setTimeout(() => {
+          this.setState({
+            saludo: data.data
+          })
+        }, 3000)
+      })
+  }
+  saveUser () {
+    Axios.post('https://replit-node-deploy--ralexrdz.repl.co/users', {
+      name: document.getElementById('user-name').value
+    }).then(response => {
+      console.log(response)
+    })
+  }
   render() {
+    console.log(this.state)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hola 12345</h1>
+        <h2>{this.state.saludo}</h2>
+        <section>
+          <input id="user-name" type="text" placeholder="name"/>
+          <button onClick={this.saveUser}>Guarda Usuario</button>
+        </section>
       </div>
     );
   }
